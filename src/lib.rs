1a6314a675dfd138f21ee8a6d6178afca6c8acf3
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-09-20 13:50:10 -0400

    📄 docs: documentation fixes and clippy satisfying

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
-use std::sync::atomic::{AtomicBool, Ordering};
+//! See [`AwaitableBool`] for all documentation.
+use core::sync::atomic::{AtomicBool, Ordering};
 
 use tokio::sync::Notify;
 
 /// A bool whose value changes can be waited on.
 ///
-/// Internally, it uses [`AtomicBool`](https://doc.rust-lang.org/std/sync/atomic/struct.AtomicBool.html)
-/// and [`tokio::sync::Notify`](https://docs.rs/tokio/latest/tokio/sync/struct.Notify.html).
+/// Internally, it uses [`AtomicBool`]
+/// and [`tokio::sync::Notify`].
 ///
 /// Because of that, most methods only need a shared reference (`&` as opposed to `&mut`) to self,
 /// so sharing between tasks or threads should be cheap and easy.
-/// This struct doesn't implement [`Clone`](https://doc.rust-lang.org/std/clone/trait.Clone.html),
-/// so place it in an [`Arc`](https://doc.rust-lang.org/std/sync/struct.Arc.html)
-/// (no lock protecting it needed) if cloning is needed.
+/// This struct doesn't implement [`Clone`], so place it in an [`Arc`]
+/// (no lock protecting it necessary per the previous sentence) if cloning is needed.
+///
+/// [`Arc`]: std::sync::Arc
+/// [`AtomicBool`]: std::sync::atomic::AtomicBool
 #[derive(Debug, Default)]
 pub struct AwaitableBool {
     bool: AtomicBool,
@@ -20,7 +23,7 @@ pub struct AwaitableBool {
 
 impl<T: Into<AtomicBool>> From<T> for AwaitableBool {
     fn from(value: T) -> Self {
-        AwaitableBool {
+        Self {
             bool: value.into(),
             notify: Notify::new(),
         }
@@ -43,11 +46,16 @@ impl AwaitableBool {
     }
 
     /// Set the `AwaitableBool` to `true`
-    /// (with [`Release`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.Release) ordering if not already `true`
-    /// and [`Acquire`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.Acquire) ordering if it is).
+    /// (with [`Release`] ordering if not already `true`
+    /// and [`Acquire`] ordering if it is).
     ///
     /// This wakes all tasks waiting for [`wait_true`].
     /// It also wakes those waiting for [`wait`] if the value wasn't already `true`.
+    ///
+    /// [`Acquire`]: core::sync::atomic::Ordering::Acquire
+    /// [`Release`]: core::sync::atomic::Ordering::Release
+    /// [`wait`]: AwaitableBool::wait
+    /// [`wait_true`]: AwaitableBool::wait_true
     pub fn set_true(&self) {
         if self
             .bool
@@ -59,11 +67,16 @@ impl AwaitableBool {
     }
 
     /// Set the `AwaitableBool` to `false`
-    /// (with [`Release`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.Release) ordering if not already `false`
-    /// and [`Acquire`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.Acquire) ordering if it is).
+    /// (with [`Release`] ordering if not already `false`
+    /// and [`Acquire`] ordering if it is).
     ///
     /// This wakes all tasks waiting for [`wait_false`].
     /// It also wakes those waiting for [`wait`] if the value wasn't already `false`.
+    ///
+    /// [`Acquire`]: core::sync::atomic::Ordering::Acquire
+    /// [`Release`]: core::sync::atomic::Ordering::Release
+    /// [`wait`]: AwaitableBool::wait
+    /// [`wait_false`]: AwaitableBool::wait_false
     pub fn set_false(&self) {
         if self
             .bool
@@ -75,11 +88,16 @@ impl AwaitableBool {
     }
 
     /// Set the `AwaitableBool` to the inverse of its current value (i.e. `false` if `true` or `true` if `false`)
-    /// (with [`AcqRel`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.AcqRel) ordering).
+    /// (with [`AcqRel`] ordering).
     ///
     /// This wakes all tasks waiting for [`wait`].
     /// It also wakes those waiting for [`wait_true`] if the value was just changed from `false` to `true`,
     /// or those waiting for [`wait_false`] if the value was just changed from `true` to `false`.
+    ///
+    /// [`AcqRel`]: core::sync::atomic::Ordering::AcqRel
+    /// [`wait`]: AwaitableBool::wait
+    /// [`wait_false`]: AwaitableBool::wait_false
+    /// [`wait_true`]: AwaitableBool::wait_true
     pub fn toggle(&self) {
         // Until AtomicBool::fetch_not is stable
         self.bool.fetch_xor(true, Ordering::AcqRel);
@@ -88,20 +106,26 @@ impl AwaitableBool {
     }
 
     /// Get the current value of the `AwaitableBool`
-    /// (with [`Acquire`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.Acquire) ordering).
+    /// (with [`Acquire`] ordering).
+    ///
+    /// [`Acquire`]: core::sync::atomic::Ordering::Acquire
     #[inline]
-    fn load(&self) -> bool {
+    pub fn load(&self) -> bool {
         self.bool.load(Ordering::Acquire)
     }
 
     /// Check if the `AwaitableBool`'s value is currently `true`
-    /// (with [`Acquire`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.Acquire) ordering).
+    /// (with [`Acquire`] ordering).
+    ///
+    /// [`Acquire`]: core::sync::atomic::Ordering::Acquire
     #[inline]
     pub fn is_true(&self) -> bool {
         self.load()
     }
     /// Check if the `AwaitableBool`'s value is currently `false`
-    /// (with [`Acquire`](https://doc.rust-lang.org/stable/core/sync/atomic/enum.Ordering.html#variant.Acquire) ordering).
+    /// (with [`Acquire`] ordering).
+    ///
+    /// [`Acquire`]: core::sync::atomic::Ordering::Acquire
     #[inline]
     pub fn is_false(&self) -> bool {
         !(self.load())
@@ -110,29 +134,35 @@ impl AwaitableBool {
     /// Wait for this [`AwaitableBool`]'s value to change.
     ///
     /// Use [`load`] after to know what it changed to.
+    ///
+    /// [`load`]: AwaitableBool::load
     pub async fn wait(&self) {
         self.notify.notified().await;
     }
 
     /// Wait for this [`AwaitableBool`]'s value to become `true`.
+    /// This returns immediately if it's already `true`.
     pub async fn wait_true(&self) {
         if self.is_false() {
             self.wait().await;
         }
     }
     /// Wait for this [`AwaitableBool`]'s value to become `false`.
+    /// This returns immediately if it's already `false`.
     pub async fn wait_false(&self) {
         if self.is_true() {
             self.wait().await;
         }
     }
 
-    /// Consume this [`AwaitableBool`] to get the contained [`AtomicBool`](https://doc.rust-lang.org/std/sync/atomic/struct.AtomicBool.html).
+    /// Consume this [`AwaitableBool`] to get the contained [`AtomicBool`].
     ///
-    /// [`AtomicBool`](https://doc.rust-lang.org/std/sync/atomic/struct.AtomicBool.html) also has
-    /// an `into_inner` method to get its contained [`bool`](https://doc.rust-lang.org/std/primitive.bool.html).
+    /// [`AtomicBool`] also has an [`into_inner`] method to get its contained [`bool`].
+    ///
+    /// [`AtomicBool`]: std::sync::atomic::AtomicBool
+    /// [`into_inner`]: std::sync::atomic::AtomicBool::into_inner
     #[inline]
-    pub fn into_inner(self) -> AtomicBool {
+    pub const fn into_inner(self) -> AtomicBool {
         self.bool
     }
 }
